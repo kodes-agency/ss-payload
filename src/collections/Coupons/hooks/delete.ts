10ab2a14a27736941f5052864cb90fe9f302e0ae
denevB <95876310+denevB@users.noreply.github.com>
2024-04-08 00:03:09 +0300

    origin

--- src/collections/Coupons/hooks/delete.ts
+++ src/collections/Coupons/hooks/delete.ts
@@ -1,31 +1,31 @@
-import { CollectionBeforeDeleteHook } from "payload/types";
-import WooCommerceRestApi from "@woocommerce/woocommerce-rest-api";
-import CustomAdminError from "../../../utilities/errorClasses";
+// import { CollectionBeforeDeleteHook } from "payload/types";
+// import WooCommerceRestApi from "@woocommerce/woocommerce-rest-api";
+// import CustomAdminError from "../../../utilities/errorClasses";
 
-export const deleteCoupon: CollectionBeforeDeleteHook = async ({ req, id }) => {
-  const WooCommerce = new WooCommerceRestApi({
-    url: process.env.PAYLOAD_PUBLIC_WOO_URL,
-    consumerKey: process.env.PAYLOAD_PUBLIC_WOO_CONSUMER_KEY,
-    consumerSecret: process.env.PAYLOAD_PUBLIC_WOO_CONSUMER_SECRET,
-    version: "wc/v3",
-  });
+// export const deleteCoupon: CollectionBeforeDeleteHook = async ({ req, id }) => {
+//   const WooCommerce = new WooCommerceRestApi({
+//     url: process.env.PAYLOAD_PUBLIC_WOO_URL,
+//     consumerKey: process.env.PAYLOAD_PUBLIC_WOO_CONSUMER_KEY,
+//     consumerSecret: process.env.PAYLOAD_PUBLIC_WOO_CONSUMER_SECRET,
+//     version: "wc/v3",
+//   });
 
-  let coupon = await req.payload.find({
-    collection: "coupons",
-    where: {
-      id: { equals: id },
-    },
-  });
+//   let coupon = await req.payload.find({
+//     collection: "coupons",
+//     where: {
+//       id: { equals: id },
+//     },
+//   });
 
-  console.log(coupon?.docs[0]?.couponId)
+//   console.log(coupon?.docs[0]?.couponId)
 
-  await WooCommerce.delete(`coupons/${coupon?.docs[0]?.couponId}`, {
-    force: true,
-  })
-    .then((response) => {
-      console.log("Coupon deleted")
-    })
-    .catch((error) => {
-      throw new CustomAdminError(error.response?.data?.message, error.response?.data?.data?.status)
-    });
-};
+//   await WooCommerce.delete(`coupons/${coupon?.docs[0]?.couponId}`, {
+//     force: true,
+//   })
+//     .then((response) => {
+//       console.log("Coupon deleted")
+//     })
+//     .catch((error) => {
+//       throw new CustomAdminError(error.response?.data?.message, error.response?.data?.data?.status)
+//     });
+// };
--- src/collections/Coupons/index.ts
+++ src/collections/Coupons/index.ts
@@ -1,102 +1,102 @@
-import type { CollectionConfig } from "payload/types";
-import { createUpdateCoupon } from "./hooks/createUpdate";
-import { deleteCoupon } from "./hooks/delete";
-import { metaData } from "./hooks/metaData";
+// import type { CollectionConfig } from "payload/types";
+// import { createUpdateCoupon } from "./hooks/createUpdate";
+// import { deleteCoupon } from "./hooks/delete";
+// import { metaData } from "./hooks/metaData";
 
-export const Coupons: CollectionConfig = {
-  slug: "coupons",
-  labels: {
-    singular: "Код за отстъпка",
-    plural: "Кодове за отстъпка",
-  },
-  admin: {
-    useAsTitle: "code",
-    defaultColumns: ["code", "discount", "updatedAt"],
-  },
-  hooks: {
-    beforeChange: [createUpdateCoupon],
-    beforeDelete: [deleteCoupon],
-    afterChange: [metaData]
-  },
-  fields: [
-    {
-      type: "row",
-      fields: [
-        {
-          name: "code",
-          type: "text",
-          label: "Code",
-          required: true,
-          unique: true,
-          admin: {
-            width: "33.33%",
-          },
-        },
-        {
-          name: "amount",
-          type: "number",
-          label: "Amount",
-          required: true,
-          min: 1,
-          admin: {
-            step: 0.01,
-            width: "33.33%",
-          },
-        },
-        {
-          name: "discount_type",
-          label: "Discount Type",
-          type: "select",
-          required: true,
-          defaultValue: 'fixed_cart',
-          admin: {
-            width: "33.33%",
-          },
-          options: [
-            { label: "Percentage", value: "percent" },
-            { label: "Fixed", value: "fixed_cart" },
-          ],
-        },
-      ],
-    },
-    {
-      type: 'number',
-      name: 'couponId',
-      label: 'Coupon ID',
-      admin: {
-        readOnly: true,
-      }
-    },
-    {
-      type: "collapsible",
-      label: "Usage Restrictions",
-      admin: {
-        initCollapsed: true,
-      },
-      fields: [
-        {
-          type: 'row',
-          fields: [
-            {
-              name: "date_expires",
-              type: "date",
-              label: "Expiry Date",
-            },
-            {
-              name: "product_ids",
-              type: "relationship",
-              label: "Only for section products",
-              relationTo: "products",
-              hasMany: true,
-            },
-            {
-              name: 'minimum_amount',
-              type: 'number',
-              label: 'Minimum Order Amount',
-            }
-          ],
-        }
-      ]
-    }
-  ],
-};
+// export const Coupons: CollectionConfig = {
+//   slug: "coupons",
+//   labels: {
+//     singular: "Код за отстъпка",
+//     plural: "Кодове за отстъпка",
+//   },
+//   admin: {
+//     useAsTitle: "code",
+//     defaultColumns: ["code", "discount", "updatedAt"],
+//   },
+//   hooks: {
+//     beforeChange: [createUpdateCoupon],
+//     beforeDelete: [deleteCoupon],
+//     afterChange: [metaData]
+//   },
+//   fields: [
+//     {
+//       type: "row",
+//       fields: [
+//         {
+//           name: "code",
+//           type: "text",
+//           label: "Code",
+//           required: true,
+//           unique: true,
+//           admin: {
+//             width: "33.33%",
+//           },
+//         },
+//         {
+//           name: "amount",
+//           type: "number",
+//           label: "Amount",
+//           required: true,
+//           min: 1,
+//           admin: {
+//             step: 0.01,
+//             width: "33.33%",
+//           },
+//         },
+//         {
+//           name: "discount_type",
+//           label: "Discount Type",
+//           type: "select",
+//           required: true,
+//           defaultValue: 'fixed_cart',
+//           admin: {
+//             width: "33.33%",
+//           },
+//           options: [
+//             { label: "Percentage", value: "percent" },
+//             { label: "Fixed", value: "fixed_cart" },
+//           ],
+//         },
+//       ],
+//     },
+//     {
+//       type: 'number',
+//       name: 'couponId',
+//       label: 'Coupon ID',
+//       admin: {
+//         readOnly: true,
+//       }
+//     },
+//     {
+//       type: "collapsible",
+//       label: "Usage Restrictions",
+//       admin: {
+//         initCollapsed: true,
+//       },
+//       fields: [
+//         {
+//           type: 'row',
+//           fields: [
+//             {
+//               name: "date_expires",
+//               type: "date",
+//               label: "Expiry Date",
+//             },
+//             {
+//               name: "product_ids",
+//               type: "relationship",
+//               label: "Only for section products",
+//               relationTo: "products",
+//               hasMany: true,
+//             },
+//             {
+//               name: 'minimum_amount',
+//               type: 'number',
+//               label: 'Minimum Order Amount',
+//             }
+//           ],
+//         }
+//       ]
+//     }
+//   ],
+// };
