11cd8389871bd6e795e215842acca0a1918fbe87
denevB <95876310+denevB@users.noreply.github.com>
2024-06-08 13:53:21 +0300

    origin

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -2,7 +2,26 @@ import { CollectionConfig } from 'payload/types'
 
 export const Media: CollectionConfig = {
   slug: 'media',
- 
+  access: {
+    read: () => true,
+  },
+  labels: {
+    singular: 'Файл',
+    plural: 'Файлове',
+  },
+  upload: {
+    staticURL: '/media',
+    staticDir: 'media',
+    adminThumbnail: 'thumbnail',
+    mimeTypes: ['image/*', 'application/pdf'],
+    formatOptions: {
+        format: 'webp',
+        options: {
+            quality: 60,
+        },
+    },
+
+  },
   fields: [
     {
       name: 'alt',
