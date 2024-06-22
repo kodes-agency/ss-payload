d4434f62aec2b6a8bda948f9d3c043ca8e847278
denevB <95876310+denevB@users.noreply.github.com>
2024-06-22 18:59:44 +0300

    origin

--- src/utilities/cardPaymentUtilities/dataValidations.ts
+++ src/utilities/cardPaymentUtilities/dataValidations.ts
@@ -2,7 +2,7 @@ import { ValidationError } from "payload/errors";
 
 
 export function validateOrderData(doc: any) {
-    if (typeof doc.ORDER !== 'string' || doc.ORDER.length !== 5) {
+    if (doc.ORDER) {
       throw new ValidationError([
         {
           field: 'ORDER',
