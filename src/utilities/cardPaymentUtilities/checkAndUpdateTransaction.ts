d13d4f9fdceba4970bc5b898f1bf711224f60262
denevB <95876310+denevB@users.noreply.github.com>
2024-06-23 12:18:07 +0300

    origin

--- src/utilities/cardPaymentUtilities/checkAndUpdateTransaction.ts
+++ src/utilities/cardPaymentUtilities/checkAndUpdateTransaction.ts
@@ -18,7 +18,7 @@ export async function checkAndUpdateTransaction(req: PayloadRequest, doc: Paymen
         const updatedTransactionData = await getBoricaData(order);
         console.log(`Checking transaction for order: ${order}. Status code: ${updatedTransactionData.RC}`);
 
-        if (transactionData.RC.includes("-")) {
+        if (!transactionData.RC.includes("-")) {
           clearInterval(intervalId);
           console.log(`Transaction RC (${updatedTransactionData.RC}) is not in the repeatCodes array anymore. Transaction recorded!`);
           await updateCardPayments(req, doc, updatedTransactionData);
